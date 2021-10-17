2d4dc0ccf6df8123f146c2d9df80acdc021f8c16
aayushie-madura <89146342+aayushie-madura@users.noreply.github.com>
2021-10-17 12:31:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage[
+    var updatedImage = ["https://i.postimg.cc/MGn9GJXw/family.jpg"];
+    var updatedImage = ["https://i.postimg.cc/qqyYvVbq/grandpa.jpg"];
+    var updatedImage = ["https://i.postimg.cc/wjMnFtMX/father.jpg"];
+    var updatedImage = ["https://i.postimg.cc/5ymDKL83/bro.jpg"];
+    var updatedImage = ["https://i.postimg.cc/JnL6wtrd/sister.jpg"];
+    var updatedImage = ["https://i.postimg.cc/bw5W5zSK/mother.jpg"];]
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName[
+    var updatedName = [Fmaily Book] ;
+    var updatedName = [Ranbir Singh] ; 
+    var updatedName = [Diljeet Singh] ;
+    var updatedName = [Rocky Singh] ;
+    var updatedName = [Alia Singh] ;
+    var updatedName = [Soni Singh] ;]
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
